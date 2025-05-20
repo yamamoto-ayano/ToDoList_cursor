ad180482e8b153c68b996e36b6b737896d9af468
aya <116853251+yamamoto-ayano@users.noreply.github.com>
2025-05-20 23:05:34 +0900

    chore: remove obsolete src files after refactor

--- src/db/client.ts
+++
@@ -1,7 +0,0 @@
-import { drizzle } from 'drizzle-orm/d1';
-import type { D1Database } from '@cloudflare/d1';
-import * as schema from './schema';
-
-export function getDrizzleClient(db: D1Database) {
-  return drizzle(db, { schema });
-} 
